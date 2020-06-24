dcb772bfffc2281b34ed8d5abfb7e9ba2a8fa608
uwaifo <mr.uwaifo@gmail.com>
2020-06-24 09:13:01 +0100

    updated model

--- src/models/assesment/assessment.js
+++ src/models/assesment/assessment.js
@@ -59,7 +59,7 @@ const assessmemtSchema = new Schema(
 
     cont_asmnt_id: {
       type: String,
-      required: true,
+      required: false,
     },
     assessment_percentage: {
       type: Number,
